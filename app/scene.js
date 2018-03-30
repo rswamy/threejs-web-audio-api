bd25932be3ed34fb995c1fb6b412c7592de1422d
Ramya Swamy <ramya@Ramyas-MacBook-Pro.local>
2018-03-30 00:47:02 -0700

    Added filters to disco

--- app/scene.js
+++ app/scene.js
@@ -86,27 +86,45 @@ class DiscoScene extends Scene2D {
 		let numberOfFlecks = 30;
 		console.log('DiscoScene] numberOfFlecks= ' + numberOfFlecks);
 		// create many flecks
+		var blurFilter1 = new PIXI.filters.BlurFilter();
+		var blurFilter2 = new PIXI.filters.BlurFilter(5);
 		for (let i = 0; i < numberOfFlecks; i++) {
 			let discoFleck = new PIXI.Graphics();
-			discoFleck.lineStyle(1, 0xffffff, 1);
+			discoFleck.beginFill(0xffffff, 0.5);
 			discoFleck.drawRect(0, 0, randomIntFromInterval(0.3 * fleckSpacing, 1.3 * fleckSpacing), 20);
+			discoFleck.endFill();
 			discoFleck.x = (i % Math.floor(windowWidth/fleckSpacing)) * randomIntFromInterval(fleckSpacing, 3 * fleckSpacing);
 			discoFleck.y = (i / 5) * fleckSpacing;
+			if(i % 2 == 0) {
+				discoFleck.filters = [blurFilter1];
+			}
+			else {
+				discoFleck.filters = [blurFilter2];
+			}
 			this.container.addChild(discoFleck);
 		}
 	}
 	audioTick(audioData) {
 		super.audioTick(audioData);
 		var volume = this.volume;
-		var childrenArray = this.container.children;
-		childrenArray.forEach(function (child) {
-			if(childrenArray.indexOf(child) % 3 == 0) {
-				child.scale.x = 1 + (volume * 10);
-			}
-			if(childrenArray.indexOf(child) % 7 == 0) {
-				child.rotation += 0.01
-			}
-		});
+		var isBeat = this.isBeat;
+		if((this.container != null) && (typeof this.container != 'undefined')) {
+			var childrenArray = this.container.children;
+			childrenArray.forEach(function (child) {
+				if(childrenArray.indexOf(child) % 3 == 0) {
+					child.scale.x = 1 + (volume * 1);
+					if(isBeat) {
+						child.fillAlpha = 1;
+					}
+					else {
+						child.fillAlpha = 0.5;
+					}
+				}
+				if(childrenArray.indexOf(child) % 2 == 0) {
+					child.rotation += 0.01
+				}
+			});
+		}
 	}
 }
 
@@ -123,11 +141,13 @@ class TriangleScene extends Scene2D {
 	}
 	audioTick(audioData) {
 		super.audioTick(audioData);
-		var volume = this.volume;
-		var childrenArray = this.container.children;
-		childrenArray.forEach(function (child) {
-			child.scale.x = 15 * volume;
-		});
+		if((this.container != null) && (typeof this.container != 'undefined')) {
+			var volume = this.volume;
+			var childrenArray = this.container.children;
+			childrenArray.forEach(function (child) {
+				child.scale.x = 15 * volume;
+			});
+		}
 	}
 }
 
@@ -260,17 +280,19 @@ class SphereScene extends Scene3D {
 	}
 	audioTick(audioData) {
 		super.audioTick(audioData);
-		// change sphere color every beat
-		var sphere = this.container.children[0];
-		if (this.isBeat) {
-			let color = this.sphereColors[this.activeColor];
-			sphere.material.color = new THREE.Color(`rgb(${color.r},${color.g},${color.b})`);
-			this.activeColor = this.activeColor < this.sphereColors.length - 1 ? this.activeColor + 1 : 0;
+		if((this.container != null) && (typeof this.container != 'undefined')) {
+			// change sphere color every beat
+			var sphere = this.container.children[0];
+			if (this.isBeat) {
+				let color = this.sphereColors[this.activeColor];
+				sphere.material.color = new THREE.Color(`rgb(${color.r},${color.g},${color.b})`);
+				this.activeColor = this.activeColor < this.sphereColors.length - 1 ? this.activeColor + 1 : 0;
+			}
+			// change sphere size based on volume
+			sphere.scale.x = .3 + (this.volume / 2);
+			sphere.scale.y = .3 + (this.volume / 2);
+			sphere.scale.z = .3 + (this.volume / 2);
 		}
-		// change sphere size based on volume
-		sphere.scale.x = .3 + (this.volume / 2);
-		sphere.scale.y = .3 + (this.volume / 2);
-		sphere.scale.z = .3 + (this.volume / 2);
 	}
 }
 
@@ -287,34 +309,36 @@ class PlaneScene extends Scene3D {
 		super.start();
 		let planePoints = 15;
 		let planeMaterial = new THREE.MeshLambertMaterial({
-			color: 0xff0000,
+			color: 0xffffff,
 			side: THREE.DoubleSide,
 			wireframe: true,
-			wireframeLinewidth: 1
+			wireframeLinewidth: 0.5
 		});
-		let planeGeometry = new THREE.PlaneGeometry(500, 500, planePoints, planePoints);
+		let planeGeometry = new THREE.PlaneGeometry(1000, 1000, planePoints, planePoints);
 		let plane = new THREE.Mesh(planeGeometry, planeMaterial);
 		// let the renderer know we plan to update the vertices
 		plane.geometry.verticesNeedUpdate = true;
 		plane.geometry.dynamic = true;
 		// rotate and position plane on ground
-		plane.position.y = -200;
-		plane.rotation.x = -Math.PI/2;
+		plane.position.z = 200;
+		plane.rotation.y = Math.PI/8;
 		this.container.add(plane);
 	}
 	audioTick(audioData) {
 		super.audioTick(audioData);
 		var waveform = this.waveform;
-		var childrenArray = this.container.children;
-		childrenArray.forEach(function (child) {
-			waveform.forEach((value, i) => {
-				if (i%2 === 0) {
-					child.geometry.vertices[i/2].z = value * 80;
-				}
+		if((this.container != null) && (typeof this.container != 'undefined')) {
+			var childrenArray = this.container.children;
+			childrenArray.forEach(function (child) {
+				waveform.forEach((value, i) => {
+					if (i%2 === 0) {
+						child.geometry.vertices[i/2].z = value * 80;
+					}
+				});
+				child.geometry.verticesNeedUpdate = true;
 			});
-			child.geometry.verticesNeedUpdate = true;
-		});
-		this.container.rotateZ(0.01);
+			this.container.rotateZ(0.001);
+		}
 	}
 }
 
@@ -325,6 +349,7 @@ class PlaneScene extends Scene3D {
  */
 class StickScene extends Scene3D {
 	start() {
+		super.start();
 		let pivot;
 		let stickEndWidth = 10;
 		let stickLength = 500;
@@ -347,19 +372,17 @@ class StickScene extends Scene3D {
 		stickGroup.position.set(0, -numberOfSticks * stickGapDistance/2, 0);
 		pivot = new THREE.Object3D();
 		pivot.add(stickGroup);
-		scene.add(pivot);
-		var rotation = { x: 0, y: 0, z:0 }; // Start at (0, 0)
-		var tween = new TWEEN.Tween(rotation) // Create a new tween that modifies 'coords'.
-			.to({ x: 0, y: Math.PI * 2, z: Math.PI * 2 }, 120000) // Move to this position in 30sec.
-			.repeat( Infinity )
-			.onUpdate(function() { // Called after tween.js updates 'coords'.
-				// Move 'box' to the position described by 'coords' with a CSS translation.
-				pivot.rotation.z = rotation.z;
-				for(let i = 0; i < stickGroup.children.length; i++) {
-					stickGroup.children[i].rotation.y = rotation.y * i;
-				}
-			})
-			.start(); // Start the tween immediately.
+		this.container.add(pivot);
+	}
+	audioTick(audioData) {
+		super.audioTick(audioData);
+		if((this.container != null) && (typeof this.container != 'undefined')) {
+			var childrenArray = this.container.children;
+			for(let i = 0; i < childrenArray.length; i++) {
+				childrenArray[i].rotateY(i * 0.2);
+			}
+			this.container.rotateZ(0.01);
+		}
 	}
 }
 
@@ -369,8 +392,10 @@ app3D.add(ambientLight);
 app3D.add(pointLight);
 
 // Create list of scenes
+let empty3Dscene = new Scene3D("empty");
 let sphereScene = new SphereScene("sphere");
 let planeScene = new PlaneScene("plane");
+let stickScene = new StickScene("stick");
 
 // When app loads, initially show the sphere scene
 var current3Dscene = sphereScene;
@@ -378,21 +403,21 @@ current3Dscene.start();
 
 // Then start choosing random 3D scenes:
 let arrayOf3Dscenes = [
-	sphereScene,
+	//sphereScene,
+	planeScene,
 	planeScene
+	// stickScene,
+	// empty3Dscene
 ];
 random3Dscene();
-
 // Choose random 3D scenes
 function random3Dscene() {
 	let idx = randomIntFromInterval(0, arrayOf3Dscenes.length - 1);
 	var next3Dscene = arrayOf3Dscenes[idx];
-
 	// Stop current scene and start next one
 	current3Dscene.stop();
 	next3Dscene.start();
 	current3Dscene = next3Dscene;
-
 	let interval3D = randomIntFromInterval(scene3DintervalMin, scene3DintervalMax);
 	scene3DtimerDisplay.innerText = millisToMinutesAndSeconds(interval3D);
 	console.log('[3D] Will change 3D scene in ' + interval3D + ' ms');
@@ -418,6 +443,9 @@ export default function(audioData) {
 	}
 	if(triangleScene != null) {
 		triangleScene.audioTick(audioData);
+	}
+	if(stickScene != null) {
+		stickScene.audioTick(audioData);
 	}
 
 	// rerender scene every update
@@ -451,4 +479,8 @@ function millisToMinutesAndSeconds(millis) {
 	var minutes = Math.floor(millis / 60000);
 	var seconds = ((millis % 60000) / 1000).toFixed(0);
 	return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function map (num, in_min, in_max, out_min, out_max) {
+	return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
